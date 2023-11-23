7ff79508e3a5fb7f66c3955c4ce84ea3db4a4f0d
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-23 16:30:27 -0500

    Matriz transpuesta

--- ejermatrizt.c
+++ ejermatrizt.c
@@ -10,13 +10,25 @@ int main()
     scanf("%d", &m);
     srand(time(NULL));
     int matrix[n][m];
+    
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             matrix[i][j]=rand()%101;
-            printf("Matriz original: \n");
+            
             printf("%d\t", matrix[i][j]);
+            
+        }
+        printf("\n");
+    }
+    int matrix2[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix2[i][j]=matrix[j][i];
+            printf("%d\t", matrix2[i][j]);
         }
         printf("\n");
     }
