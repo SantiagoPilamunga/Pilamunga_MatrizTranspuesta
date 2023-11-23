1ea8b9a46426df84b26983fe3f7de250b7e3424c
Santiago <santiago.pilamunga@udla.edu.ec>
2023-11-23 16:05:26 -0500

    Dimensiones de la matriz

--- ejermatrizt.c
+++ ejermatrizt.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    
+    int n, m;
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &n);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &m);
+    int matrix[n][m];
     return 0;
 }
